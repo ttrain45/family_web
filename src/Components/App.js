50acd4d6c37558cb45341a8074931ff455369b5a
Tyler Trainor <ttrainor@captechventures.com>
2018-10-17 18:13:51 -0400

    Initial Commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../StyleSheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>Initial Upload</React.Fragment>
+    );
+  }
+}
+
+export default App;
